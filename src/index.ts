abe49faaa9295f952318314574cca28f43af98a0
Jamie White <jamie@jgwhite.co.uk>
2021-09-07 11:45:14 +0200

    Start adding TS code generation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-import { CodeGeneratorRequest, CodeGeneratorResponse } from "./proto/plugin";
+import { builders as b } from "ast-types";
 import { readFileSync } from "fs";
+import { print } from "recast";
+
+import { CodeGeneratorRequest, CodeGeneratorResponse } from "./proto/plugin";
 
 const stdin = readFileSync(0);
 const request = CodeGeneratorRequest.deserializeBinary(stdin);
@@ -7,7 +10,24 @@ const request = CodeGeneratorRequest.deserializeBinary(stdin);
 const response = new CodeGeneratorResponse();
 
 const file = new CodeGeneratorResponse.File();
+
+const ast = b.program([
+  b.variableDeclaration("let", [
+    b.variableDeclarator(
+      {
+        ...b.identifier("name"),
+        typeAnnotation: b.tsTypeAnnotation(
+          b.tsTypeReference(b.identifier("string"))
+        ),
+      },
+      b.stringLiteral("alice")
+    ),
+  ]),
+]);
+
 file.setName("hello.ts");
+file.setContent(print(ast).code);
+
 response.addFile(file);
 
 process.stdout.write(response.serializeBinary());
